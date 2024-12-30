f7601fa727dd117c14dd1c18e34f66fee280b360
AV-016 <sbaka5201@gmail.com>
2024-12-31 01:19:34 +0530

    main

---
+++ pythonf1telemetry.py
@@ -0,0 +1,11 @@
+
+
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as pt 
+laps=pd.DataFrame(pd.read_csv('/home/anushka/Downloads/laps.csv'))
+flaps=pd.DataFrame(pd.read_csv('/home/anushka/Downloads/Fatest_Laps.csv'))
+
+
+
+
